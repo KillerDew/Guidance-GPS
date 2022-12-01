b739f912276642891543c7345c8ace2407a5ac91
KillerDew <106707099+KillerDew@users.noreply.github.com>
2022-12-01 12:11:34 +0000

    HaverSine Linear interpolation method
    --Algorithm Summary--
    1) determine 3D vector in metres between two gps points [found in GetVector()]
    
    2)extract extra data from vector(distance)
    
    3)Lerp along vector by x [in Lerp()]
    
    4) add lerped vector to initial point [addmetres()]
    
    ----
    
    *NOTE*
    algorithm only accurate to about 20-300 metres, this is because the HaverSine formulae only works on perfect spheres - where as the earth is ellipsoid
    Vincenty formulae to be implemented.
    **

---
+++ haversineLerp.py
@@ -0,0 +1,79 @@
+import math
+import time
+
+earthrad = 6364.912*1000
+class GeoLocation:
+    Latitude = 0.0000
+    Longitude = 0.0000
+    ALtitude = 0.0000
+    def __init__(self, lat, longi, alt):
+        self.Latitude = lat
+        self.Longitude = longi
+        self.ALtitude = alt
+
+    def __str__(self):
+        string = "{latitude} {longitude} {alti}".format(latitude=self.Latitude, longitude = self.Longitude, alti = self.ALtitude)
+        return string
+
+    def __add__(a, b):
+        return GeoLocation((a.Latitude + b.Latitude), (a.Longitude+b.Longitude), (a.ALtitude+b.Latitude))
+    def __times__(self, x):
+        return GeoLocation((self.Latitude*x), (self.Longitude*x), (self.ALtitude*x))
+
+    def __minus__(self, y):
+        new = GeoLocation(self.Latitude-y.Latitude, self.Longitude-y.Longitude, self.ALtitude-y.Altitude)
+        return new
+
+    def AddMetres(self, x):
+        new = GeoLocation(0, 0, 0)
+        new.Latitude = self.Latitude+(180/math.pi)*(x.Longitude/earthrad)
+        new.Longitude = self.Longitude+(180/math.pi)*(x.Latitude/earthrad)/math.cos(self.Latitude)
+        new.ALtitude = self.ALtitude
+        return new
+
+
+
+def GetVector (x, y):
+    b = y.ALtitude + earthrad
+    c = x.ALtitude + earthrad
+
+    b2 = b**2
+    c2 = c**2
+    bc2 = 2*b*c
+
+    alpha = y.Longitude - x.Longitude
+    alpha = alpha *(math.pi/180)
+    
+    outy = (b2 +c2 - bc2*math.cos(alpha))**0.5
+
+    alpha = y.Latitude - x.Latitude
+    alpha = alpha *(math.pi/180)
+    
+    outx = (b2+c2-bc2*math.cos(alpha))**0.5
+
+    outz = b-c
+
+    return GeoLocation(outx, outy, outz)
+
+def Lerp(a, b, x):
+    new = GeoLocation.__add__(a, b)
+    return new.__times__(x)
+
+
+
+
+
+in1 = input("Launch coordinate [lat, long, alt]: ")
+launch = GeoLocation(float(in1.split(", ")[0]), float(in1.split(", ")[1]), int(in1.split(", ")[2]))
+time.sleep(0.5)
+in2 = input("Target coordinate [lat, long, alt]: ")
+targ = GeoLocation(float(in2.split(", ")[0]), float(in2.split(", ")[1]), int(in2.split(", ")[2]))
+time.sleep(0.5)
+vector = GetVector(launch, targ)
+midvector = Lerp(GeoLocation(0, 0, launch.ALtitude), vector, 0.5)
+output = launch.AddMetres(midvector)
+print("Calculating")
+time.sleep(1)
+print (vector)
+print (output)
+print ((vector.Latitude**2+vector.Longitude**2+vector.ALtitude**2)**0.5)
